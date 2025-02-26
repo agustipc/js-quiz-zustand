ffc0d925652f839152a68c537dd4e971f4102536
Agustí Pou Company <agustipcom@gmail.com>
2025-02-26 10:55:21 +0100

    Persist data

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,24 @@
+import { Button, Stack } from '@mui/material'
 import { useQuestionsData } from '../hooks/useQuestionsData'
+import { useQuestions } from '../store/questions'
 
 export const Footer = () => {
   const { correct, incorrect, unanswered } = useQuestionsData()
+  const reset = useQuestions((state) => state.reset)
 
   return (
     <footer style={{ marginTop: '16px' }}>
-      <strong>{`✅ ${correct} Correct - ❌ ${incorrect} Incorrect - ❓${unanswered} Unanswered`}</strong>
+      <Stack spacing={6}>
+        <strong>{`✅ ${correct} Correct - ❌ ${incorrect} Incorrect - ❓${unanswered} Unanswered`}</strong>
+        <Button
+          type="reset"
+          variant="outlined"
+          color="error"
+          onClick={() => reset()}
+        >
+          Reset
+        </Button>
+      </Stack>
     </footer>
   )
 }
--- src/store/questions.ts
+++ src/store/questions.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand'
 import { type Question } from '../types.d'
+import { persist, devtools } from 'zustand/middleware'
 
 interface QuestionsState {
   questions: Question[]
@@ -8,57 +9,81 @@ interface QuestionsState {
   selectAnswer: (questionId: number, answerIndex: number) => void
   goToNextQuestion: () => void
   goToPreviousQuestion: () => void
+  reset: () => void
 }
 
-export const useQuestions = create<QuestionsState>((set, get) => {
-  return {
-    questions: [],
-    currentQuestion: 0,
-    loading: false,
+export const useQuestions = create<QuestionsState>()(
+  devtools(
+    persist(
+      (set, get) => {
+        return {
+          questions: [],
+          currentQuestion: 0,
+          loading: false,
 
-    fetchQuestions: async (limit: number) => {
-      const response = await fetch('http://localhost:5173/data.json')
-      const data = await response.json()
+          fetchQuestions: async (limit: number) => {
+            const response = await fetch('http://localhost:5173/data.json')
+            const data = await response.json()
 
-      const questions = data.sort(() => Math.random() - 0.5).slice(0, limit)
-      set({ questions })
-    },
+            const questions = data
+              .sort(() => Math.random() - 0.5)
+              .slice(0, limit)
+            set({ questions }, false, 'FETCH_QUESTIONS')
+          },
 
-    selectAnswer: (questionId: number, answerIndex: number) => {
-      const { questions } = get()
-      const newQuestions = structuredClone(questions)
+          selectAnswer: (questionId: number, answerIndex: number) => {
+            const { questions } = get()
+            const newQuestions = structuredClone(questions)
 
-      const questionIndex = newQuestions.findIndex(
-        (question) => question.id === questionId
-      )
-      const questionInfo = newQuestions[questionIndex]
-      const isCorrectAnswer = questionInfo.correctAnswer === answerIndex
+            const questionIndex = newQuestions.findIndex(
+              (question) => question.id === questionId
+            )
+            const questionInfo = newQuestions[questionIndex]
+            const isCorrectAnswer = questionInfo.correctAnswer === answerIndex
 
-      newQuestions[questionIndex] = {
-        ...questionInfo,
-        isCorrectUserAnswer: isCorrectAnswer,
-        userSelectedAnswer: answerIndex
+            newQuestions[questionIndex] = {
+              ...questionInfo,
+              isCorrectUserAnswer: isCorrectAnswer,
+              userSelectedAnswer: answerIndex
+            }
+
+            set({ questions: newQuestions }, false, 'SELECT_ANSWER')
+          },
+
+          goToNextQuestion: () => {
+            const { currentQuestion, questions } = get()
+            const nextQuestion = currentQuestion + 1
+
+            if (nextQuestion < questions.length) {
+              set(
+                { currentQuestion: nextQuestion },
+                false,
+                'GO_TO_NEXT_QUESTION'
+              )
+            }
+          },
+
+          goToPreviousQuestion: () => {
+            const { currentQuestion } = get()
+            const previousQuestion = currentQuestion - 1
+
+            if (previousQuestion >= 0) {
+              set(
+                { currentQuestion: previousQuestion },
+                false,
+                'GO_TO_PREVIOUS_QUESTION'
+              )
+            }
+          },
+
+          reset: () => {
+            set({ questions: [], currentQuestion: 0 }, false, 'RESET')
+          }
+        }
+      },
+      {
+        name: 'questions-storage'
       }
-
-      set({ questions: newQuestions })
-    },
-
-    goToNextQuestion: () => {
-      const { currentQuestion, questions } = get()
-      const nextQuestion = currentQuestion + 1
-
-      if (nextQuestion < questions.length) {
-        set({ currentQuestion: nextQuestion })
-      }
-    },
-
-    goToPreviousQuestion: () => {
-      const { currentQuestion } = get()
-      const previousQuestion = currentQuestion - 1
-
-      if (previousQuestion >= 0) {
-        set({ currentQuestion: previousQuestion })
-      }
-    }
-  }
-})
+    )
+  )
+)
